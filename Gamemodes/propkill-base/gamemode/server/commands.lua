6e52da2a56538e9c61e037bc138215a33bc077ad
AlmightyLaxz <almightylaxz@gmail.com>
2016-05-21 13:52:35 +0930

    removed serverside rotate fml

--- Gamemodes/propkill-base/gamemode/server/commands.lua
+++ Gamemodes/propkill-base/gamemode/server/commands.lua
@@ -4,14 +4,3 @@ concommand.Add("rserver", function(ply)
 		RunConsoleCommand("changelevel", game.GetMap(), "propkill")
 	end
 end)
-
-concommand.Add("ms_rotate", function(ply)
-	local a = ply:EyeAngles()
-	ply:SetEyeAngles(Angle(a.p, a.y-180, a.r))
-end)
-
-concommand.Add("ms_rotate2", function(ply)
-	local a = ply:EyeAngles() ply:SetEyeAngles(Angle(a.p-a.p-a.p, a.y-180, a.r))
-	ply:ConCommand("+jump")
-	timer.Simple(0.2, function() ply:ConCommand("-jump") end)
-end)
