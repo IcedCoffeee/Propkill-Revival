ce6900eae6e103e4a1dfeaa6ecc551387f6af5a6
Iced Coffee <me@icedd.coffee>
2017-05-03 18:54:22 +1000

    removed double defintion and optimised team select

--- Gamemodes/propkill-base/gamemode/server/commands.lua
+++ Gamemodes/propkill-base/gamemode/server/commands.lua
@@ -1,12 +1,9 @@
 function ChangeTeam(ply, cmd, args)
 	local teamindex = tonumber(args[1])
-
-	for k,v in pairs(team.GetAllTeams()) do
-		if tostring(teamindex) == tostring(k) && k < 1000 then
-			ChatMsg({team.GetColor(ply:Team()), ply:Nick(), cwhite, " has joined team ", team.GetColor(teamindex), team.GetName(teamindex), "!"})
-			ply:SetTeam(teamindex)
-			ply:Spawn()
-		end
+	if teamindex < 1000 and team.Valid(teamindex) then
+		ply:SetTeam(teamindex)
+		ChatMsg({team.GetColor(ply:Team()), ply:Nick(), cwhite, " has joined team ", team.GetColor(teamindex), team.GetName(teamindex), "!"})
+		ply:Spawn()
 	end
 end
 concommand.Add("pk_team", ChangeTeam)
--- Gamemodes/propkill-tdm/gamemode/server/commands.lua
+++ Gamemodes/propkill-tdm/gamemode/server/commands.lua
@@ -1,24 +1,3 @@
-function ChangeTeam(ply, cmd, args)
-	if args[1] == "0" then
-		ChatMsg({team.GetColor(ply:Team()), ply:Nick(), Color(255,255,255), " has joined team ", team.GetColor(3), "Spectator!"})
-		ply:SetTeam(TEAM_UNASSIGNED)
-		ply:Spawn()
-	end
-	
-	if args[1] == "1" then
-		ChatMsg({team.GetColor(ply:Team()), ply:Nick(), Color(255,255,255), " has joined team ", team.GetColor(1), "Red!"})
-		ply:SetTeam(TEAM_RED)
-		ply:Spawn()
-	end
-
-	if args[1] == "2" then
-		ChatMsg({team.GetColor(ply:Team()), ply:Nick(), Color(255,255,255), " has joined team ", team.GetColor(2), "Blue!"})
-		ply:SetTeam(TEAM_BLUE)
-		ply:Spawn()
-	end
-end
-concommand.Add("pk_team", ChangeTeam)
-
 function AutoBalance(ply)
 	if !ply:IsSuperAdmin() then return false end
 	LogPrint("Balancing and shuffling teams...")
