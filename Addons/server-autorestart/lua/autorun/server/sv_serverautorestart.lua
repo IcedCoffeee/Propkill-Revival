c1ad17e2c0ca41895578e546fe1356397fe81604
Iced Coffee <me@icedd.coffee>
2019-04-29 13:40:16 +1000

    timers dont run with no players online

--- Addons/server-autorestart/lua/autorun/server/sv_serverautorestart.lua
+++
@@ -1,9 +0,0 @@
--- Automatically restarts the server at 6AM if there are no players
-
-hook.Add("InitPostEntity", "sv_autorestart", function()
-	timer.Create("sv_autorestart", math.abs(os.date("%H")-6)*60*60, 1, function()
-		if #player.GetAll() == 0 then
-			RunConsoleCommand("changelevel", game.GetMap(), engine.ActiveGamemode())
-		end
-	end)
-end)
